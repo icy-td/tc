3d6adac211ea9691396ebd5cb2602bd1c6d03d24
icy-td <15257070+icy-td@users.noreply.github.com>
2019-10-18 13:37:09 -0400

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -26,7 +26,7 @@
                 <li><a href="/">Home</a></li>
                 <li><a href="/posts">Charts</a></li>
                 <li><a href="/about">About</a></li>
-                <li><span style="display:block;"><a href="https://twitter.com/search?q=%24tslaq"><img src="https://abs.twimg.com/favicons/twitter.ico" border="0"> $TSLAQ</a></span></li>
+                <li><div style="display:block;"><a href="https://twitter.com/search?q=%24tslaq"><img src="https://abs.twimg.com/favicons/twitter.ico" border="0">$TSLAQ</a></div></li>
             </ul>
         </nav>
     </header>
