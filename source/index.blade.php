050fc21ae49af0e657ea2b58d4be65e10c617b64
icy-td <15257070+icy-td@users.noreply.github.com>
2019-10-23 04:12:41 -0400

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -5,6 +5,8 @@
 
     <p>In case you didn't know, "TC" as we Robinhood types like to call him, has an extensive background in solar technology, earned a Ph.D. in a related field, and likes to drink Coors Light on occasion. Other information about the man is spotty by design, but we do encourage you listen to his guest podcasts on Quoth the Raven.</p>
 
-    <p><span class="test-css"><blockquote class="twitter-tweet" data-theme="light"><p lang="en" dir="ltr">The mission of <a href="https://twitter.com/TESLAcharts?ref_src=twsrc%5Etfw">@TeslaCharts</a> is to unabashedly serve the confirmation bias of <a href="https://twitter.com/hashtag/Tesla?src=hash&amp;ref_src=twsrc%5Etfw">#Tesla</a> bears. We flow with the content curation current, providing exactly what bears most want to see. Especially helpful on days the stock goes up on bad news. Enjoy accordingly...</p>&mdash; TeslaCharts (@TESLAcharts) <a href="https://twitter.com/TESLAcharts/status/963119114804977667?ref_src=twsrc%5Etfw">February 12, 2018</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></span></p>
+    <span class="test-css">
+    <blockquote class="twitter-tweet" data-theme="light"><p lang="en" dir="ltr">The mission of <a href="https://twitter.com/TESLAcharts?ref_src=twsrc%5Etfw">@TeslaCharts</a> is to unabashedly serve the confirmation bias of <a href="https://twitter.com/hashtag/Tesla?src=hash&amp;ref_src=twsrc%5Etfw">#Tesla</a> bears. We flow with the content curation current, providing exactly what bears most want to see. Especially helpful on days the stock goes up on bad news. Enjoy accordingly...</p>&mdash; TeslaCharts (@TESLAcharts) <a href="https://twitter.com/TESLAcharts/status/963119114804977667?ref_src=twsrc%5Etfw">February 12, 2018</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </span>
 
 @endsection
