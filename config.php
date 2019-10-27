5234b7c80a89bc3778d883e620f361e97981a8a3
icy-td <15257070+icy-td@users.noreply.github.com>
2019-10-27 13:29:47 -0400

    Update config.php

--- config.php
+++ config.php
@@ -36,7 +36,7 @@ return [
     ],
     'excerpt' => function ($page, $limit = 250, $end = '...') {
         return $page->isPost
-            ? str_limit_soft(content_sanitize($page->getDescription()), $limit, $end)
+            ? str_limit_soft(content_sanitize($page->getContent()), $limit, $end)
             : null;
     },
     'imageCdn' => function ($page, $path) {
