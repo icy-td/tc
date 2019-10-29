00f5e92c477afbd179979d77f35913ecc7b97358
icy-td <15257070+icy-td@users.noreply.github.com>
2019-10-29 17:07:06 -0400

    Update meta.blade.php

--- source/_partials/head/meta.blade.php
+++ source/_partials/head/meta.blade.php
@@ -5,14 +5,14 @@
 
 <meta property="og:title" content="{{ $page->title ?: $page->site->title }}">
 <meta property="og:description" content="{!! $page->excerpt() ?: $page->site->description !!}">
-<meta property="og:image" content="{{ $page->imageCdn($page->image ? $page->image : $page->site->image) }}">
+<meta property="og:image" content="https://teslacharts.net/tc.png">
 <meta property="og:type" content="{{ $page->isPost ? 'article' : 'website' }}">
 <meta property="og:url" content="{{ $page->getUrl() }}">
 
 <meta name="twitter:title" content="{{ $page->title ?: $page->site->title }}">
 <meta name="twitter:description" content="{!! $page->excerpt() ?: $page->site->description !!}">
-<meta name="twitter:image" content="{{ $page->imageCdn($page->image ? $page->image : $page->site->image) }}">
-<meta name="twitter:card" content="summary">
+<meta name="twitter:image" content="https://teslacharts.net/tc.png">
+<meta name="twitter:card" content="Repository for all @TESLACharts charts about Tesla stock price, Tesla bonds, Tesla earnings, Tesla registrations, Model S and Model X sales, Tesla deliveries, etc.">
 <meta name="twitter:site" content="{{ "@{$page->owner->twitter}" }}">
 <meta name="twitter:creator" content="{{ "@{$page->owner->twitter}" }}">
 <!-- end search/social -->
