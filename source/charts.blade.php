f96b710513a8a59a11ab14e5520b981f22483906
icy-td <15257070+icy-td@users.noreply.github.com>
2019-11-12 23:42:51 -0500

    Update charts.blade.php

--- source/charts.blade.php
+++ source/charts.blade.php
@@ -8,7 +8,7 @@
     <ul>
         @forelse ($posts->sortBy('date') as $post)
             <li>
-                <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
+                <a href="{{ $post->getPath() }}/">{{ $post->title }}</a>
                 <small>{{ date('M j, Y', $post->date) }}</small>
             </li>
         @empty
